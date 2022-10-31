a9ce7245ecd93957ad33796451070bbfdcf675a5
Lisa Koivu <lisamarie.koivu@gmail.com>
2022-10-31 10:13:12 -0400

    first cut at serverless v2 postgres stack. Includes differentiation between reader and writer instances.

---
+++ main.tf
@@ -0,0 +1,146 @@
+locals {
+  master_username = "root"
+}
+
+data "aws_kms_key" "db" {
+  key_id = var.kms_key_alias
+}
+
+data "aws_rds_engine_version" "postgresql" {
+  engine  = "aurora-postgresql"
+  version = var.postgres_engine_version
+}
+
+resource "aws_rds_cluster" "cluster" {
+  skip_final_snapshot              = true
+  cluster_identifier               = var.cluster_identifier
+  engine                           = data.aws_rds_engine_version.postgresql.engine
+  engine_mode                      = "provisioned"
+  database_name                    = var.cluster_identifier
+  kms_key_id                       = data.aws_kms_key.db.arn
+  storage_encrypted                = true
+  tags                             = var.tags
+  master_username                  = local.master_username
+  master_password                  = random_password.db.result
+  db_cluster_parameter_group_name  = aws_rds_cluster_parameter_group.cluster.name
+  db_instance_parameter_group_name = aws_db_parameter_group.db.name
+  performance_insights_enabled     = var.performance_insights_enabled
+  db_subnet_group_name             = aws_db_subnet_group.cluster.name
+  vpc_security_group_ids           = []
+  serverlessv2_scaling_configuration {
+    max_capacity = var.max_capacity
+    min_capacity = var.min_capacity
+  }
+}
+
+resource "aws_rds_cluster_instance" "serverless" {
+  count                        = var.reader_instance_count
+  cluster_identifier           = aws_rds_cluster.cluster.cluster_identifier
+  identifier_prefix            = "${aws_rds_cluster.cluster.cluster_identifier}-"
+  instance_class               = "db.serverless"
+  engine                       = aws_rds_cluster.cluster.engine
+  engine_version               = aws_rds_cluster.cluster.engine_version
+  db_subnet_group_name         = aws_db_subnet_group.cluster.name
+  db_parameter_group_name      = aws_db_parameter_group.db.name
+  performance_insights_enabled = var.performance_insights_enabled
+  promotion_tier               = 2
+}
+
+resource "aws_rds_cluster_instance" "provisioned" {
+  count                        = var.writer_instance_count
+  cluster_identifier           = aws_rds_cluster.cluster.cluster_identifier
+  identifier_prefix            = "${aws_rds_cluster.cluster.cluster_identifier}-"
+  instance_class               = "db.t3.medium"
+  engine                       = aws_rds_cluster.cluster.engine
+  engine_version               = aws_rds_cluster.cluster.engine_version
+  db_subnet_group_name         = aws_db_subnet_group.cluster.name
+  db_parameter_group_name      = aws_db_parameter_group.db.name
+  performance_insights_enabled = var.performance_insights_enabled
+  promotion_tier               = 0
+}
+
+resource "aws_db_parameter_group" "db" {
+  name        = var.cluster_identifier
+  family      = var.family
+  description = "Terraform managed instance parameter group for ${var.cluster_identifier}"
+  dynamic "parameter" {
+    for_each = var.db_instance_parameters
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+      apply_method = parameter.value.apply_method
+    }
+  }
+  tags = merge(var.tags, var.db_parameter_group_tags)
+}
+
+resource "aws_rds_cluster_parameter_group" "cluster" {
+  name        = var.cluster_identifier
+  family      = var.family
+  description = "Terraform managed cluster parameter group for ${var.cluster_identifier}"
+  dynamic "parameter" {
+    for_each = var.rds_cluster_parameters
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+      apply_method = parameter.value.apply_method
+    }
+  }
+  tags = merge(var.tags, var.rds_cluster_parameter_group_tags)
+}
+
+resource "aws_db_subnet_group" "cluster" {
+  name       = var.cluster_identifier
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_secretsmanager_secret" "db" {
+  name_prefix = "database/${aws_rds_cluster.cluster.cluster_identifier}/master-"
+  description = "Master password for ${aws_rds_cluster.cluster.cluster_identifier}. Managed by Terraform."
+  tags        = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "db" {
+  secret_id = aws_secretsmanager_secret.db.id
+  secret_string = jsonencode({
+    host     = aws_rds_cluster.cluster.endpoint
+    port     = aws_rds_cluster.cluster.port
+    dbname   = aws_rds_cluster.cluster.cluster_identifier
+    username = aws_rds_cluster.cluster.master_username
+    password = random_password.db.result
+
+  })
+}
+
+resource "random_password" "db" {
+  length  = 12
+  special = true
+}
+
+resource "aws_security_group" "db" {
+  name   = var.cluster_identifier
+  vpc_id = var.vpc_id
+  tags   = var.tags
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_cidr_blocks
+  }
+
+  # TODO Lock this down later
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = var.ingress_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = var.egress_cidr_blocks
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,137 @@
+variable "cluster_identifier" {
+  description = "The cluster identifier for the cluster to be created."
+  type        = string
+}
+
+variable "db_instance_parameters" {
+  description = "Map of parameters to configure the database instance resource."
+  type = list(object({
+    name         = string
+    value        = string
+    apply_method = string
+  }))
+  default = []
+}
+
+variable "db_parameter_group_tags" {
+  description = "A map of tags to add to the db_parameter_group resource."
+  default     = {}
+}
+
+
+# variable "performance_insights_enabled" {
+#   description = "Whether or not to enable performance insights for this db."
+#   type        = bool
+#   default     = false
+# }
+
+variable "egress_cidr_blocks" {
+  description = "A list of CIDR blocks which are allowed to access the database"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "family" {
+  description = "The database family of the instance and parameter group."
+  default     = "aurora-postgresql13"
+}
+
+variable "ingress_cidr_blocks" {
+  description = "A list of CIDR blocks which are allowed to access the database"
+  type        = list(string)
+  default     = ["10.0.0.0/8"]
+}
+
+variable "kms_key_alias" {
+  description = "The alias of the KMS encryption key. When specifying kms_key_id, storage_encrypted needs to be set to true."
+  type        = string
+}
+
+variable "max_capacity" {
+  description = "The maxiumum capacity for an Aurora DB cluster in serverless DB engine mode. Must be greater than or equal to minimum capacity"
+}
+
+variable "min_capacity" {
+  description = "The minimum capacity for an Aurora DB cluster in serverless DB engine mode. Must be less than or equal to the maximum capacity."
+  type        = number
+  default     = 2
+}
+
+variable "performance_insights_enabled" {
+  description = "Switch to turn on performance insights."
+  type        = bool
+  default     = false
+}
+
+variable "performance_insights_retention_period" {
+  description = "How many days to keep Performance Insights data."
+  type        = number
+  default     = 7
+}
+
+variable "port" {
+  description = "The port the instance listens on."
+  type        = number
+  default     = 5432
+}
+
+variable "postgres_engine_version" {
+  description = "The number of writer instances to create."
+  type        = string
+  default     = "13.6"
+}
+
+variable "preferred_backup_window" {
+  description = "When to perform DB backups"
+  type        = string
+  default     = "09:00-10:00" # 2AM-3AM MST
+}
+
+variable "preferred_maintenance_window" {
+  description = "When to perform DB maintenance"
+  type        = string
+  default     = "sat:07:00-sat:09:00" # 12AM-2AM MST
+}
+
+variable "rds_cluster_parameter_group_tags" {
+  description = "A map of tags to add to the aws_rds_cluster_parameter_group resource if one is created."
+  default     = {}
+}
+
+variable "rds_cluster_parameters" {
+  description = "Map of parameters to configure the cluster resource."
+  type = list(object({
+    name         = string
+    value        = string
+    apply_method = string
+  }))
+  default = []
+}
+
+variable "reader_instance_count" {
+  description = "The number of reader instances to create."
+  type        = number
+  default     = 1
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs to use"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to provision into"
+  type        = string
+}
+
+variable "writer_instance_count" {
+  description = "The number of writer instances to create."
+  type        = number
+  default     = 1
+}
